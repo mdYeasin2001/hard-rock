1374e4f21cfa71809ffc94caf5adac3f86253155
Yeasin <yeasin30112001@gmail.com>
2021-02-20 19:52:43 -0600

    get lyrics and show on browser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const searchValue = document.getElementById('search-box');
 document.getElementById('search-btn').addEventListener('click', () => {
-    console.log(searchValue.value);
     fetch(`https://api.lyrics.ovh/suggest/${searchValue.value}`)
     .then(response => response.json())
     .then(data => getSongs(data.data))
@@ -9,7 +8,7 @@ const getSongs = (songs) => {
     const songsContainer = document.getElementById('songs-container');
     songsContainer.innerHTML = '';
     songs.map(song => {
-        console.log(song.artist.name);
+        // console.log(song);
         const eachSong = `
                 <div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9">
@@ -18,10 +17,23 @@ const getSongs = (songs) => {
                         <audio src="${song.preview}" controls></audio>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick="getLyrics('${song.title}', '${song.artist.name}')" class="btn btn-success">Get Lyrics</button>
                     </div>
                 </div>
         `
         songsContainer.innerHTML += eachSong;
     })
+}
+
+const getLyrics = (songTitle, songArtistName) => {
+    fetch(`https://api.lyrics.ovh/v1/${songArtistName}/${songTitle}`)
+    .then(response => response.json())
+    .then(data => showLyrics(data.lyrics))
+}
+const showLyrics = (lyrics) => {
+    const lyricsContainer = document.getElementById('lyrics-container');
+    const lyricsText = `
+    <pre class="text-white">${lyrics}</pre>
+    `
+    lyricsContainer.innerHTML = lyricsText;
 }
